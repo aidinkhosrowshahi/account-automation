2e116016522f0ff1c00907cf4eb8672eb7b43f7d
Aidin khosrowshahi <32849802+aidinkhosrowshahi@users.noreply.github.com>
2024-04-02 16:50:04 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install AWS SAM CLI
+curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+
+# Verify SAM installation
+where sam
+sam —version
+
+# Install Python 3.10.14
+wget https://www.python.org/ftp/python/3.10.14/Python-3.10.14.tgz
+tar -xf Python-3.10.14.tgz
+cd Python-3.10.14
+./configure --enable-optimizations
+sudo make altinstall
+
